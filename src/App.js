c55529e80e76163cb8009e68a3540c58e6b5defa
gauravsinhaweb <sinhagaurav.me@gmail.com>
2021-05-03 23:05:57 +0530

    'commit'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Chart from "./Chart";
 function App() {
+  // const [res, setRes] = useState([]);
+
+  // let socket = new WebSocket("wss://stream.binance.com:9443/ws/btcusdt@trade");
+
+  // useEffect(() => {
+  //   socket.onmessage = function (event) {
+  //     setRes(JSON.parse(event.data));
+  //   };
+  // }, [socket]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2 style={{ textAlign: "center" }}>Challenge</h2>
+      <Chart />
+    </>
   );
 }
 
